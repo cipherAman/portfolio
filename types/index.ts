30fe59cf3238b8d971d16cb29deaabc5db17bda6
Cipher <aman.enthusiast@gmail.com>
2026-05-27 16:49:36 +0530

    Defined types in intex.ts

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,36 @@
 // TYpes 
 
-cd
+export interface Project{
+    id:string;
+    title:string;
+    description:string;
+    longDescription:string;
+    technologies:string[];
+    image:string;
+    github?:string;
+    live?:string;
+    featured:boolean;
+
+}
+
+
+export interface Skill {
+    id:string;
+    category:string;
+    items:string[];
+}
+
+export interface Experience{
+    id:string;
+    company:string;
+    role:string;
+    period:string;
+    description:string;
+    skills:string[];
+}
+
+export interface ContactFormData{
+    name:string;
+    email:string;
+    message:string;
+}
